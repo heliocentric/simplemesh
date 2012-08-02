9229bf11dd401bf059b4ae2a0a08bce0458eabdf
Dylan Cochran <heliocentric@gmail.com>
2012-08-02 18:10:43 -0400

    * Bump to 0.2

--- c#/smesh-lib/Properties/Resources.Designer.cs
+++ c#/smesh-lib/Properties/Resources.Designer.cs
@@ -70,7 +70,7 @@ namespace SimpleMesh.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 0.1.13.
+        ///   Looks up a localized string similar to 0.2.1.
         /// </summary>
         internal static string ServiceVersion {
             get {
