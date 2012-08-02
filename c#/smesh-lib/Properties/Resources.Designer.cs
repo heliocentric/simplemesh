324d4299ff6f9979b0bb974ad7fde4d43f08ab49
Dylan Cochran <heliocentric@gmail.com>
2012-08-02 16:53:33 -0400

    * Fix using.

--- c#/smesh-lib/Properties/Resources.Designer.cs
+++ c#/smesh-lib/Properties/Resources.Designer.cs
@@ -70,7 +70,7 @@ namespace SimpleMesh.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 0.1.10.
+        ///   Looks up a localized string similar to 0.1.11.
         /// </summary>
         internal static string ServiceVersion {
             get {
